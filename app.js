95760d2da7ca97253585b7a9d3ffb86d9eddd360
Nikita Titov <nmtitov@yandex.ru>
2012-04-12 13:35:45 +0400

    Added json api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http')
 	, util = require('util');
 
+var port = process.env['NODE_PORT']||8080;
+
 var optionsFrom = {
 	host: 'deathstar',
 	port: 8001,
@@ -8,59 +10,55 @@ var optionsFrom = {
 	method: 'GET',
 };
 
-function map(items, func) {
-	var todo = items.concat();
-
-	function handle() {
-		func(todo.shift());
-		if(todo.length > 0) {
-			process.nextTick(handle);
-		}
-	}
-
-	handle();
-}
-
-
-var req = http.request(optionsFrom, function(res) {
-	var data = '';
-	res.setEncoding('utf8');
-	res.on('data', function(chunk) {
-		data += chunk;
-	});
-
-	res.on('end', function() {
-		var entries = data.split('\n\n');
-		map(entries, function(entry) {
-			var lines = entry.split('\n');
-			if (lines.length == 3) {
-				var splitDate = lines[0].split(' ');
-				var stringDate = splitDate[2] + ' ' + splitDate[1] + ' ' + splitDate[5] + ' ' + splitDate[3];
-
-				var date = new Date();
-				date.setTime(Date.parse(stringDate));
-
-				var package = {};
-				package["date"] = date;
-
-				var splitTemp1 = lines[1].split(' ');
-				var temp1 = splitTemp1[8];
-				package["processor"] = temp1;
-
-				var splitTemp2 = lines[2].split(' ');
-				var temp2 = splitTemp2[8];
-				package["inside"] = temp2;
-
-				console.log(util.inspect(package));
-			};
+http.createServer(function (ureq, ures) {
+	var req = http.request(optionsFrom, function(res) {
+		var data = '';
+		res.setEncoding('utf8');
+		res.on('data', function(chunk) {
+			data += chunk;
 		});
 
+		res.on('end', function() {
+			var entries = data.split('\n\n');
+			var response = [];
+
+			for (var i = entries.length - 1; i >= 0; i--) {
+				var entry = entries[i];
+				var lines = entry.split('\n');
+				if (lines.length == 3) {
+					var splitDate = lines[0].split(' ');
+					var stringDate = splitDate[2] + ' ' + splitDate[1] + ' ' + splitDate[5] + ' ' + splitDate[3];
+
+					var date = new Date();
+					date.setTime(Date.parse(stringDate));
+
+					var package = {};
+					package["date"] = date;
+
+					var splitTemp1 = lines[1].split(' ');
+					var temp1 = splitTemp1[8];
+					package["processor"] = temp1;
+
+					var splitTemp2 = lines[2].split(' ');
+					var temp2 = splitTemp2[8];
+					package["inside"] = temp2;
+
+					response.push(package);
+				};
+			};
+
+			ures.writeHead(200, {'Content-Type': 'text/json'});
+			ures.write(JSON.stringify(response));
+			ures.end('\n');
+
+		});
+	})
+
+	req.on('error', function(e) {
+		console.log('Problem with request: ' + e.message);
 	});
-})
 
-req.on('error', function(e) {
-	console.log('Problem with request: ' + e.message);
-});
+	req.end();
+}).listen(port);
 
-req.end();
-
+console.log('Started server');
